7e14526abfdc14f899caece29abea285155e8924
thienphuc205 <aronthienphuc@gmail.com>
2025-08-20 19:05:06 +0700

    Bai Tap 1

--- main.dart
+++ main.dart
@@ -15,53 +15,54 @@ Chương trình sẽ:
 4. In kết quả:
 ○ Họ tên, tổng lương trước thuế, thuế thu nhập, và lương thực lãnh
 */
-// void main() {
-//   print("--- Chương trình tính lương ---");
+void main() {
+  print("--- Chương trình tính lương ---");
 
-//   // Tạo một đối tượng nhân viên
-//   var nhanVien1 = NhanVien("Nguyen Hoang Thien Phuc", 45, 180000);
-//   nhanVien1.tinhTongLuong();
+  // Tạo một đối tượng nhân viên
+  var nhanVien1 = NhanVien("Nguyen Hoang Thien Phuc", 45, 180000);
+  nhanVien1.tinhTongLuong();
 
-//   // Tạo thêm một nhân viên khác để kiểm tra
-//   var nhanVien2 = NhanVien("Pham Van Vu", 35, 250000);
-//   nhanVien2.tinhTongLuong();
-// }
+  // Tạo thêm một nhân viên khác để kiểm tra
+  var nhanVien2 = NhanVien("Pham Van Vu", 35, 250000);
+  nhanVien2.tinhTongLuong();
+}
 
-// class NhanVien {
-//   //Nhap thong tin
-//   String hoTen;
-//   double gioLamViec;
-//   double luongMoiGio;
+class NhanVien {
+  //Nhap thong tin
+  String hoTen;
+  double gioLamViec;
+  double luongMoiGio;
 
-//   NhanVien(this.hoTen, this.gioLamViec, this.luongMoiGio);
+  NhanVien(this.hoTen, this.gioLamViec, this.luongMoiGio);
 
-//   //Tinh tong luong nhan duoc
-//   void tinhTongLuong() {
-//     //Luong truoc thue
-//     double luongTruocThue = this.gioLamViec * this.luongMoiGio;
+  //Tinh tong luong nhan duoc
+  void tinhTongLuong() {
+    //Luong truoc thue
+    double luongTruocThue = this.gioLamViec * this.luongMoiGio;
 
-//     // Them phu cap
-//     if (this.gioLamViec > 40) {
-//       luongTruocThue = luongTruocThue + (luongTruocThue * 0.2);
-//     }
+    // Them phu cap
+    if (this.gioLamViec > 40) {
+      luongTruocThue = luongTruocThue + (luongTruocThue * 0.2);
+    }
 
-//     //Thue thu nhap
-//     double thueThuNhap = luongTruocThue > 10000000
-//         ? luongTruocThue * 0.1
-//         : luongTruocThue >= 7000000 && luongTruocThue <= 10000000
-//         ? luongTruocThue * 0.05
-//         : 0;
+    //Thue thu nhap
+    double thueThuNhap = luongTruocThue > 10000000
+        ? luongTruocThue * 0.1
+        : luongTruocThue >= 7000000 && luongTruocThue <= 10000000
+        ? luongTruocThue * 0.05
+        : 0;
 
-//     //Luong cuoi cung nhan duoc
-//     double luongThucLanh = luongTruocThue - thueThuNhap;
+    //Luong cuoi cung nhan duoc
+    double luongThucLanh = luongTruocThue - thueThuNhap;
 
-//     //In ket qua
-//     print("Bảng lương của nhân viên ${this.hoTen} ---");
-//     print("Tổng lương trước thuế: ${luongTruocThue.toStringAsFixed(0)} VND");
-//     print("Thuế thu nhập: ${thueThuNhap.toStringAsFixed(0)} VND");
-//     print("Lương thực lãnh: ${luongThucLanh.toStringAsFixed(0)} VND");
-//   }
-// }
+    //In ket qua
+    print("Bảng lương của nhân viên ${this.hoTen}");
+    print("Tổng lương trước thuế: ${luongTruocThue.toStringAsFixed(0)} VND");
+    print("Thuế thu nhập: ${thueThuNhap.toStringAsFixed(0)} VND");
+    print("Lương thực lãnh: ${luongThucLanh.toStringAsFixed(0)} VND");
+    print('----------------');
+  }
+}
 
 /*
 Đề bài:
@@ -80,61 +81,77 @@ Chương trình sẽ:
 ○ Tên sản phẩm, số lượng, đơn giá.
 ○ Thành tiền, giảm giá, thuế VAT, và tổng thanh toán cuối cùng.
 */
-void main() {
-  print("--- Hóa Đơn Thanh Toán ---");
+// void main() {
+//   print("--- Hóa Đơn Thanh Toán ---");
 
-  // Tạo hóa đơn 1
-  var hoadon1 = HoaDon("My Ly Cung Dinh", 5, 16000);
-  hoadon1.tongTien();
+//   // Tạo hóa đơn 1
+//   var hoadon1 = HoaDon("My Ly Cung Dinh", 5, 16000);
+//   hoadon1.tongTien();
 
-  // Tạo hóa đơn 2
-  var hoadon2 = HoaDon("Bánh mì trứng", 1, 12000);
-  hoadon2.tongTien();
-}
+//   // Tạo hóa đơn 2
+//   var hoadon2 = HoaDon("Bánh mì trứng", 1, 12000);
+//   hoadon2.tongTien();
+// }
 
-class HoaDon {
-  //Nhap thong tin hoa don
-  String tenSanPham;
-  int soLuongMua;
-  double donGia;
+// class HoaDon {
+//   //Nhap thong tin hoa don
+//   String tenSanPham;
+//   int soLuongMua;
+//   double donGia;
 
-  HoaDon(this.tenSanPham, this.soLuongMua, this.donGia);
+//   HoaDon(this.tenSanPham, this.soLuongMua, this.donGia);
 
-  // Tong tien can tra
-  void tongTien() {
-    //So tien truoc khi giam gia
-    double tienTruocGiamGia = this.soLuongMua * this.donGia;
-    double tienSauGiamGia;
+//   // Tong tien can tra
+//   void tongTien() {
+//     //So tien truoc khi giam gia
+//     double tienTruocGiamGia = this.soLuongMua * this.donGia;
+//     double tienSauGiamGia;
 
-    //So tien khi ap dung giam gia
-    if (tienTruocGiamGia >= 1000000) {
-      tienSauGiamGia = tienTruocGiamGia - tienTruocGiamGia * 0.1;
-    } else if (tienTruocGiamGia >= 500000 && tienTruocGiamGia < 1000000) {
-      tienSauGiamGia = tienTruocGiamGia - tienTruocGiamGia * 0.05;
-    } else {
-      tienSauGiamGia = tienTruocGiamGia;
-    }
+//     //So tien khi ap dung giam gia
+//     if (tienTruocGiamGia >= 1000000) {
+//       tienSauGiamGia = tienTruocGiamGia - tienTruocGiamGia * 0.1;
+//     } else if (tienTruocGiamGia >= 500000 && tienTruocGiamGia < 1000000) {
+//       tienSauGiamGia = tienTruocGiamGia - tienTruocGiamGia * 0.05;
+//     } else {
+//       tienSauGiamGia = tienTruocGiamGia;
+//     }
 
-    //Tinh thue VAT
-    double thueVAT = tienSauGiamGia * 0.08;
+//     //Tinh thue VAT
+//     double thueVAT = tienSauGiamGia * 0.08;
 
-    //Tinh tong thanh toan cuoi cung
-    double tongTienThanhToan = tienSauGiamGia + thueVAT;
+//     //Tinh tong thanh toan cuoi cung
+//     double tongTienThanhToan = tienSauGiamGia + thueVAT;
 
-    //In ket qua
-    print("Tên sản phẩm: ${this.tenSanPham} ");
-    print("Số lượng: ${this.soLuongMua}");
-    print("Đơn giá ${this.donGia.toStringAsFixed(0)} VND");
-    print(
-      "Tổng tiền trước giảm giá: ${tienTruocGiamGia.toStringAsFixed(0)} VND",
-    );
-    print(
-      "Giảm giá ${(tienTruocGiamGia - tienSauGiamGia).toStringAsFixed(0)} VND",
-    );
-    print("Tổng tiền sau giảm giá: ${tienSauGiamGia.toStringAsFixed(0)} VND");
-    print("Thuế VAT ${thueVAT.toStringAsFixed(0)} VND");
-    print(
-      "Tổng thanh toán cuối cùng ${tongTienThanhToan.toStringAsFixed(0)} VND ---",
-    );
-  }
-}
+//     //In ket qua
+//     print("Tên sản phẩm: ${this.tenSanPham} ");
+//     print("Số lượng: ${this.soLuongMua}");
+//     print("Đơn giá ${this.donGia.toStringAsFixed(0)} VND");
+//     print(
+//       "Tổng tiền trước giảm giá: ${tienTruocGiamGia.toStringAsFixed(0)} VND",
+//     );
+//     print(
+//       "Giảm giá ${(tienTruocGiamGia - tienSauGiamGia).toStringAsFixed(0)} VND",
+//     );
+//     print("Tổng tiền sau giảm giá: ${tienSauGiamGia.toStringAsFixed(0)} VND");
+//     print("Thuế VAT ${thueVAT.toStringAsFixed(0)} VND");
+//     print(
+//       "Tổng thanh toán cuối cùng ${tongTienThanhToan.toStringAsFixed(0)} VND ---",
+//     );
+//   }
+// }
+
+/* 
+Đề bài:
+Viết chương trình quản lý danh sách sinh viên gồm các chức năng sau:
+Thêm sinh viên vào danh sách. Mỗi sinh viên có:
+Họ tên.
+Điểm toán.
+Điểm lý.
+Điểm hóa.
+Hiển thị danh sách sinh viên kèm theo:
+Điểm trung bình (ĐTB).
+Xếp loại học lực dựa trên ĐTB: 
+Dưới 5: Kém, 5 - 7: Khá, 7 - 9: Giỏi, lớn hơn 9: Xuất sắc
+Tìm sinh viên có ĐTB cao nhất trong danh sách.
+Thoát chương trình.
+*/
